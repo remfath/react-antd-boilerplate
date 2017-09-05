0f49e85c6455f8d87d71dc3edc7036779fc1239c
remfath <remfath@gmail.com>
2017-09-05 22:22:43 +0800

    Set .env.production file for build enviroment and .env for develop enviroment

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,19 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
 
 module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
-    config.plugins.push(new Dotenv());
+
+    if(process.env.NODE_ENV === 'production') {
+        config.plugins.push(new Dotenv({
+            path: path.join(__dirname, '.env.production')
+        }));
+    } else {
+        config.plugins.push(new Dotenv({
+            path: path.join(__dirname, '.env')
+        }));
+    }
+
     return config;
 };
